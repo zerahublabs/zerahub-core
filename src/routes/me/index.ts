911ab44c890149e87e8a4d030fb2a5818c93ca2e
billalxcode <billal.xcode@gmail.com>
2025-06-03 23:14:45 +0700

    feat(routes): add profile route to retrieve user information with authentication feat(profile.ts): create profile route handler to fetch user data from database

--- src/routes/me/index.ts
+++ src/routes/me/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import collection from "./collection";
+import profile from "./profile"
 
 const app = new Hono();
 
 app.route("/collections", collection);
+app.route("/profile", profile)
 
 export default app;
---
+++ src/routes/me/profile.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@/generated/prisma";
+import { authMiddleware } from "@/middlewares/auth";
+import { Hono } from "hono";
+
+const prisma = new PrismaClient();
+const app = new Hono();
+
+app.use(authMiddleware);
+
+app.get("/", async (c) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            address: c.get("user").address,
+        },
+    });
+
+    return c.json({
+        status: "ok",
+        data: user,
+    });
+});
+
+export default app;
