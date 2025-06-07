e36313a30290e7d8a0c139cbcb47d73a25ab5927
billalxcode <billal.xcode@gmail.com>
2025-06-07 15:04:25 +0700

    refactor(user.ts): simplify username update route by removing zodValidator middleware and directly parsing request body style(user.ts): adjust indentation for consistency and readability in the code

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,17 +4,15 @@ import { Hono } from "hono";
 import { z } from "zod";
 import { getProfile, updateUsername } from "./user.docs";
 import { errorMiddleware } from "@/middlewares/error";
-import { zodValidator } from "@/middlewares/validator";
 
 const prisma = new PrismaClient();
 
 const updateUsernameSchema = z.object({
-	username: z.string().min(1),
+    username: z.string().min(1),
 });
 
 type Variables = {
-	user: User;
-	validated: z.infer<typeof updateUsernameSchema>;
+    user: User;
 };
 
 const app = new Hono<{ Variables: Variables }>();
@@ -22,40 +20,35 @@ const app = new Hono<{ Variables: Variables }>();
 app.use(authMiddleware);
 
 app.get("/me", getProfile, async (c) => {
-	const user = c.get("user");
+    const user = c.get("user");
 
-	return c.json({
-		status: "ok",
-		data: user,
-	});
+    return c.json({
+        status: "ok",
+        data: user,
+    });
 });
 
-app.post(
-	"/username",
-	updateUsername,
-	zodValidator(updateUsernameSchema),
-	async (c) => {
-		const validatedData = c.get("validated");
-		const user = c.get("user");
+app.post("/username", updateUsername, async (c) => {
+    const validatedData = updateUsernameSchema.parse(await c.req.json());
+    const user = c.get("user");
 
-		const updatedUser = await prisma.user.update({
-			where: {
-				address: user.address,
-			},
-			data: {
-				username: validatedData.username,
-				updatedAt: new Date(),
-			},
-		});
+    const updatedUser = await prisma.user.update({
+        where: {
+            address: user.address,
+        },
+        data: {
+            username: validatedData.username,
+            updatedAt: new Date(),
+        },
+    });
 
-		return c.json({
-			status: "ok",
-			data: {
-				username: updatedUser.username,
-			},
-		});
-	},
-);
+    return c.json({
+        status: "ok",
+        data: {
+            username: updatedUser.username,
+        },
+    });
+});
 
 app.onError(errorMiddleware);
 export default app;
